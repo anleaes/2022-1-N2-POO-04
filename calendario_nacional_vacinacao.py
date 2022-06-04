99792b5895e686e3bcefb0c71dfb1df284c529df
LucasLesnicki <lucaslesnicki@gmail.com>
2022-06-04 16:51:03 -0300

    reajuste do calendário nacional

--- calendario_nacional_vacinacao.py
+++ calendario_nacional_vacinacao.py
@@ -1,4 +1,6 @@
-class CalendarioNacionalVacinacao:
+from carteirinha_de_vacinacao import Carteirinha_de_vacinacao
+
+class CalendarioNacionalVacinacao(Carteirinha_de_vacinacao):
     def __init__(self, crianca, adolescente, adulto, idosos):
         self.crianca = crianca
         self.adolescente = adolescente
