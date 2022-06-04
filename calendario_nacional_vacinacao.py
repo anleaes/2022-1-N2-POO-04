47ec535617b6b83ff633b78756b78d413152a618
LucasLesnicki <lucaslesnicki@gmail.com>
2022-06-04 17:01:47 -0300

    mudança da carteirinha no diagrama e ajuste na classe Calendario_nacional_vacinacao

--- calendario_nacional_vacinacao.py
+++ calendario_nacional_vacinacao.py
@@ -1,6 +1,4 @@
-from carteirinha_de_vacinacao import Carteirinha_de_vacinacao
-
-class CalendarioNacionalVacinacao(Carteirinha_de_vacinacao):
+class CalendarioNacionalVacinacao:
     def __init__(self, crianca, adolescente, adulto, idosos):
         self.crianca = crianca
         self.adolescente = adolescente
