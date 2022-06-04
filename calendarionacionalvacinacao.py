e105d0a6833ed2d0d4a50cef190fe077c17d0b3c
diegocomelli <diegocomellidasilveira@gmail.com>
2022-06-04 15:41:13 -0300

    criado arquivos classes --pessoa-vacinas-posto-calendarionacional

---
+++ calendarionacionalvacinacao.py
@@ -0,0 +1,7 @@
+class CalendarioNacionalVacinacao:
+    def __init__(self, crianca, adolescente, adulto, idosos):
+        self.crianca = crianca
+        self.adolescente = adolescente
+        self.adulto = adulto
+        self.idosos = idosos
+        
---
+++ vacinas.py
@@ -0,0 +1,8 @@
+class Vacinas:
+    def __init__(self, nome, data_fabricacao, dose, fabricante, doenca_tratar):
+        self.nome = nome
+        self.data_fabricacao = data_fabricacao
+        self.dose = dose
+        self.fabricante = fabricante
+        self.doenca_tratar = doenca_tratar
+        
