89771576c80471802d903c8eee7a6e5954ddf7f6
diegocomelli <diegocomellidasilveira@gmail.com>
2022-06-07 01:46:54 -0300

    metodos em pessoa e funcionario + criado arquivo para comandos

---
+++ comandos_rodar_programa.py
@@ -0,0 +1,1 @@
+#comandos arquivo externo 
--- funcionario.py
+++ funcionario.py
@@ -4,3 +4,8 @@ class Funcionario(Pessoa):
     def __init__(self, nome, cpf, rg, idade, matricula):
         super(Funcionario,self).__init__(nome, cpf, rg, idade)
         self.matricula = matricula
+        
+        
+    def ver_nome(self):
+        print (self._pessoa._nome)
+        
