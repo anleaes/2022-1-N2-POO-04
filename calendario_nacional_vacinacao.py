f084d3caeccea2d00c3d092e631114da46b54673
diegocomelli <diegocomellidasilveira@gmail.com>
2022-06-07 17:21:40 -0300

    na classe calendario nacional de vacinação criei o método get e set

--- calendario_nacional_vacinacao.py
+++ calendario_nacional_vacinacao.py
@@ -5,3 +5,53 @@ class CalendarioNacionalVacinacao:
         self.adulto = adulto
         self.idosos = idosos
         
+    
+
+    @property
+    def get_crianca(self):
+        return self._crianca
+
+    @get_crianca.setter
+    def set_crianca(self, crianca):
+        self._crianca = crianca
+    
+    @property
+    def get_adolescente(self):
+        return self._adolescente
+
+    @get_adolescente.setter
+    def set_adolescente(self, adolescente):
+        self._adolescente = adolescente
+    
+    @property
+    def get_adulto(self):
+        return self._adulto
+
+    @get_adulto.setter
+    def set_adulto(self, adulto):
+        self._adulto = adulto
+        
+    @property
+    def get_idosos(self):
+        return self._idosos
+
+    @get_idosos.setter
+    def set_idosos(self, idosos):
+        self._idosos = idosos
+        
+calendario2022 = CalendarioNacionalVacinacao ('janeiro - 0 a 12anos',' março - 12 a 17anos','abril 18 a 59anos',' maio 60 a 99anos' )
+
+print (calendario2022.crianca)
+
+calendario2022.set_crianca = input("digite nova data de vacinação: ")
+
+print(calendario2022.get_crianca)
+
+
+
+
+
+
+            
+
+    
