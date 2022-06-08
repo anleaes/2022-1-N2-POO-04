e6eff05d91e898a82e77e2026b9f9dd006e32210
diegocomelli <diegocomellidasilveira@gmail.com>
2022-06-08 01:39:58 -0300

    editado classe pessoa e arquivo comandos_rodar_programa, testado método

--- comandos_rodar_programa.py
+++ comandos_rodar_programa.py
@@ -6,14 +6,17 @@
 
 from pessoa import *
 from endereco import *
-from funcionario import *
+#from funcionario import *
 
 
 
-e1 = Endereco (9879879, 'bento gonçalves', 'vila nova', 'canoas')
-p1 = Pessoa(91790248, 'ipiranga 59', 'jardim vila nova', 'poa', 'DIEGO',99999,6546,36)
-p1.chamar_nome()
-p1.chamar_endereco_rua()
+#e1 = Endereco (9879879, 'bento gonçalves', 'vila nova', 'canoas')
+#p1 = Pessoa(91790248, 'ipiranga 59', 'jardim vila nova', 'poa', 'DIEGO',99999,6546,36)
+#p1.chamar_nome()
+#p1.chamar_endereco_rua()
+
+
+p1.endereco_casa_pessoa()
 
 
 
--- pessoa.py
+++ pessoa.py
@@ -1,7 +1,7 @@
 from endereco import Endereco
 
 class Pessoa(Endereco):
-    def __init__(self, cep,rua,bairro,cidade, nome,cpf, rg, idade):
+    def __init__(self, nome,cpf, rg, idade, cep,rua,bairro,cidade, ):
         super(Pessoa,self) .__init__(cep,rua,bairro,cidade)
         self.nome = nome
         self.cpf = cpf
@@ -9,8 +9,11 @@ class Pessoa(Endereco):
         self.idade = idade
     
     def endereco_casa_pessoa(self):
-        return print("endereço da rua da %i ", self.nome, "é %i", self.rua)
+        return print("endereço da rua da", self.nome, "é: ", self.rua)
     
 
-p1 = Pessoa('veronica', '400.289.22', 558893, 18)
-p2 = Pessoa('greice', '5573676', 558893, 33)
+p1 = Pessoa('veronica', '400.289.22', 558893, 18,'91780-999', 'ipiranga 241', 'vila nova', 'poa')
+p2 = Pessoa('greice', '5573676', 558893, 33,'88888-999', 'cap. amarantes xavier 241', 'campo novo', 'canoas')
+
+#mano aqui quando chamo o metodo, pede para nós declarar todos os atributos
+#inclusive os atributos que estão dentro da super classe (cep,rua,bairro,cidade)
