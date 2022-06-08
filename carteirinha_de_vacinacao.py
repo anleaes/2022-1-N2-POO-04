2847be26fb25d6c0dd5c2b0f60a3f62f6afdb1aa
LucasLesnicki <lucaslesnicki@gmail.com>
2022-06-07 21:22:24 -0300

    atualização do sistema

--- carteirinha_de_vacinacao.py
+++ carteirinha_de_vacinacao.py
@@ -4,7 +4,7 @@ from calendario_nacional_vacinacao import CalendarioNacionalVacinacao
 
 
 class Carteirinha_de_vacinacao(Pessoa, Vacinas, CalendarioNacionalVacinacao):
-    def __init__(self, nome, cpf, rg, idade, nome_vacina, dose, fabricante, doenca_tratar, crianca, adolescente, adulto, idosos):
+    def __init__(self, nome, cpf, rg, idade, nome_vacina, dose, fabricante, doenca_tratar, crianca, adolescente, adulto, idosos, registro_carteirinha):
         super(Carteirinha_de_vacinacao,self).__init__(nome, cpf, rg, idade)
         super(Carteirinha_de_vacinacao,self).__init__(self, nome_vacina, dose, fabricante, doenca_tratar)
         super(Carteirinha_de_vacinacao,self).__init__(self, crianca, adolescente, adulto, idosos)
